1b8097711fae7db2bd9262d3b42d238981805825
PatzPaul <pathefister@gmail.com>
2021-12-15 00:18:14 +0300

    Detailed the css and wrote Js to the page
    got issues with adding and manipulation of countdown sequence

--- scriptsrocket.js
+++ scriptsrocket.js
@@ -1,4 +1,34 @@
-var glutton = function (name){
-    alert('enter the name:' +name )
+var timer = null;
+var countdownNumber = 10;
 
+var changeState = function (State){
+    document.body.className = 'body-State' +State;
+    clearInterval =(timer);
+    countdownNumber = 10;
+    document.getElementById=('Countdown').innerHTML = countdownNumber;
+
+
+//COUNTDOWN 
+    if (State == 4){
+timer = setInterval (function(){
+    countdownNumber = countdownNumber-1; 
+    document.getElementById=('Countdown').innerHTML = countdownNumber;
+
+ if (countdownNumber <=0){
+    changeState(5)
+}
+},500);
+
+}
+else if (State == 5){ 
+    var success = setTimeout(function(){
+var randomNumber= Math.round(Math.random()*10);
+        console.log('randomNumber:',randomNumber);
+if (randomNumber > 2 ){
+    changeState(4);
+}else{
+    changeState(6);
+}
+    },2000);
+};
 }
