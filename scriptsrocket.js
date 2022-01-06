66e88e43dfab995059ccfd7d4c801da2d4528d2d
PatzPaul <pathefister@gmail.com>
2022-01-06 15:22:41 +0300

    re edited js and html to try and make a story

--- scriptsrocket.js
+++ scriptsrocket.js
@@ -1,34 +1,39 @@
 var timer = null;
 var countdownNumber = 10;
 
+
+
 var changeState = function (State){
     document.body.className = 'body-State' +State;
-    clearInterval =(timer);
+    clearInterval(timer);
     countdownNumber = 10;
     document.getElementById=('Countdown').innerHTML = countdownNumber;
 
-
+//targets
 //COUNTDOWN 
     if (State == 4){
 timer = setInterval (function(){
     countdownNumber = countdownNumber-1; 
     document.getElementById=('Countdown').innerHTML = countdownNumber;
 
- if (countdownNumber <=0){
-    changeState(5)
+ if (countdownNumber <= 0){
+    changeState(3)
 }
 },500);
 
 }
-else if (State == 5){ 
-    var success = setTimeout(function(){
+//random execution number
+else if (State == 3){ 
+    success = setTimeout(function(){
 var randomNumber= Math.round(Math.random()*10);
         console.log('randomNumber:',randomNumber);
-if (randomNumber > 2 ){
-    changeState(4);
+if (randomNumber <=4 ){
+    changeState(5);
 }else{
     changeState(6);
 }
     },2000);
 };
+
+
 }
